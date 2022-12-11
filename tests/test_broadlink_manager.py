bfe9ad542ee52a71fb2b36e80e25348134bce9cc
gpongelli <842026+gpongelli@users.noreply.github.com>
2022-12-11 16:56:05 +0100

    new test for broadlink manager

---
+++ tests/test_broadlink_manager.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2022 Gabriele Pongelli
+#
+# SPDX-License-Identifier: MIT
+
+"""Test Broadlink manager module."""
+
+import pytest
+from unittest.mock import Mock, patch
+from broadlink_listener.cli_tools.broadlink_manager import BroadlinkManager
+
+
+class TestBroadlinkManager:
+    """Broadlink manager test class."""
+
+    @patch('broadlink.device.Device.auth', Mock(return_value=True))
+    @patch('broadlink.gendevice')
+    def test_init(self):
+        """Test initialization."""
+        _ = BroadlinkManager('0x1234', '192.168.1.1', '12345678')
+
+        with pytest.raises(ValueError):
+            _ = BroadlinkManager('0x1234', '192.168.1.1', '1234567ssss8')
+
+        with pytest.raises(ValueError):
+            _ = BroadlinkManager('abcs', '192.168.1.1', '12345678')
